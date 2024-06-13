9e14c98b4c1e34a400e6b5b8580465e84cbb66ca
Linux <linux@kali.kali>
2024-06-13 19:22:11 +0500

    API error is handeled. It means if for any reason API does not work than site will display a message for the user.

--- Text_To_Voice/core/views.py
+++ Text_To_Voice/core/views.py
@@ -14,17 +14,26 @@ def TextToVoice(request):
     if request.method == 'POST':
         text = request.POST.get('text', '').strip()
         language = request.POST.get('language', '').strip()
-        if text:
-            # Generate audio in memory
-            buffer = BytesIO()
-            sound = gtts.gTTS(text, lang=language)
-            sound.write_to_fp(buffer)
-            buffer.seek(0)
+        try:
+            if text:
+                # Generate audio in memory
+                buffer = BytesIO()
+                sound = gtts.gTTS(text, lang=language)
+                sound.write_to_fp(buffer)
+                buffer.seek(0)
 
-            # Set download headers for attachment
-            filename = 'your_audio_filename.mp3'  # Replace with desired filename
-            response = HttpResponse(buffer.getvalue(), content_type='audio/mpeg')
-            response['Content-Disposition'] = f'attachment; filename="{filename}"'
+                # Set download headers for attachment
+                filename = 'your_audio_filename.mp3'  # Replace with desired filename
+                response = HttpResponse(buffer.getvalue(), content_type='audio/mpeg')
+                response['Content-Disposition'] = f'attachment; filename="{filename}"'
 
-            return response
+                return response
+        except:
+            template = "API_error.html"
+            context = {
+                "error": "Error",
+                'message': "Sorry, we are facing an isue during converting your text please check your internet connection and try again."
+            }
+            return render(request, template, context)
+
     return redirect('/')
