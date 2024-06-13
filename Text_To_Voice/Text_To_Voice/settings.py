009a1c361d83a605cd14394b71d385ef1ede3e09
Linux <linux@kali.kali>
2024-06-13 18:55:24 +0500

    DB settings are added as commit becouse we don't need DB for this project.

--- Text_To_Voice/Text_To_Voice/settings.py
+++ Text_To_Voice/Text_To_Voice/settings.py
@@ -71,15 +71,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Text_To_Voice.wsgi.application'
 
 
+
+# For this project we don't need db.
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
